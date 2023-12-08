944ec33cadf3afc74f16bd0375357097cdc94c8d
Anmolkannaujiya <149555965+Anmolkannaujiya@users.noreply.github.com>
2023-12-08 22:44:16 +0530

    Create calculator1.py

---
+++ calculator1.py
@@ -0,0 +1,11 @@
+x=int(input("x:"))
+y=int(input("y:"))
+oper=input("operator:")
+if oper =="+":
+    print(x+y)
+elif oper=="-":
+    print(x-y)
+elif oper=="*":
+    print(x*y)
+elif oper=="/":
+    print(x/y)
